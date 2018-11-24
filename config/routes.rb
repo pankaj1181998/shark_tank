5b5ab00ad31be879e7d3e3b947215719182ac960
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-11-24 18:46:10 +0530

    company link edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'result/filtere'
   resources :all_sharks
-   root :to => 'result#filtere'
+  root :to => 'result#filtere'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
